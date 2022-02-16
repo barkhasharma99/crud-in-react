b705196d1a50f401299ce4bf7dd83ec98b6a07d9
barkha sharma <barkha.sharma55722@gmail.com>
2022-02-16 18:25:16 +0530

    added

--- src/components/Adduser.js
+++ src/components/Adduser.js
@@ -1,8 +1,55 @@
+import { Button, FormControl, FormGroup, Input, InputLabel  , makeStyles, Typography } from "@material-ui/core";
+import { setUsers , useState } from "react";
 
 
+const useStyle = makeStyles({
+    container : {
+        width :'50%',
+        margin: ' 5% 0 0 25%',
+        ' & > *' : {
+            margin: 20 
+        }
+
+
+
+    }
+})
+const initialvalues ={
+    name : '',
+    email  : '',
+    phone : '',
+     
+}
+
+
+  
 const Adduser = () => {
+
+    const [users , setUsers] = useState(initialvalues );
+    const { name , email, phone}= users;
+    const classes = useStyle();
     return (
-        <p>hello add user</p>
+       <FormGroup className={ classes.container}>
+           <Typography variant="h4"> ADD User</Typography>
+           <FormControl>
+               <InputLabel>S no.</InputLabel>
+               <Input />
+           </FormControl>
+           <FormControl>
+               <InputLabel>Name</InputLabel>
+               <Input />
+           </FormControl>
+           <FormControl>
+               <InputLabel>Email</InputLabel>
+               <Input />
+           </FormControl>
+           <FormControl>
+               <InputLabel>Phone</InputLabel>
+               <Input />
+           </FormControl>
+           <Button variant="contained" color="primary">ADD USER</Button>
+       </FormGroup>
+       
     )
 }
 export default Adduser;
--- src/components/Alluser.js
+++ src/components/Alluser.js
@@ -1,8 +1,69 @@
+import { TableBody, TableCell, TableHead, TableRow ,Table , makeStyles} from "@material-ui/core";
+import { useEffect  , useState } from "react";
+import { getUser } from "../service/api";
+
+const useStyle = makeStyles({
+    table : {
+        width:'90%'
+    },
+    thead :{
+        ' & > *' : {
+            background : '#000000 ',
+            color : '#ffffff',
+            fontSize: '20px'
+    },
+    row : {
+        ' & > * ' : {
+            fontSize : 20
+        }
+    } 
+    }
+})
 
 
 const Alluser = () => {
+ 
+     const [users , setUsers] = useState([]);
+     const classes = useStyle();
+
+    useEffect ( () => {
+        getAllUser();
+    },[])
+   
+   const getAllUser = async () => {
+       const response = await getUser();
+       console.log(response.data);
+       setUsers(response.data); 
+   }
+   
+   
+   
+   
+   
     return (
-        <p>hello all users</p>
+ 
+        <Table className={classes.table}>
+            <TableHead>
+                <TableRow className={ classes.thead}>
+                    <TableCell>id</TableCell>
+                    <TableCell>name</TableCell>
+                    <TableCell>email</TableCell>
+                    <TableCell>phone</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+               {
+               users.map(users => (
+                <TableRow className={classes.row }  style={{ fontSize:20}} >
+                <TableCell>{users.id}</TableCell>
+                <TableCell>{users.name}</TableCell>
+                <TableCell>{users.email}</TableCell>
+                <TableCell>{users.phone}</TableCell>
+            </TableRow>
+               )) 
+               }
+            </TableBody>
+        </Table>
     )
 }
 export default Alluser;
